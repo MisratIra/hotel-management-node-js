9fc01152e6fdf04af918179766789a6d9a06118a
MisratIra <misratmehenaj199@gmail.com>
2024-05-02 09:07:08 +0600

    Information Modified

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,11 +6,11 @@ const fs = require("fs");
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "hotel", // Change this to your PostgreSQL username
-  host: "localhost",
-  database: "hotel",
-  password: "hotel", // Change this to your PostgreSQL password
-  port: 5432, // Default PostgreSQL port
+	user: "hotel", // Change this to your PostgreSQL username
+	host: "localhost",
+	database: "hotel",
+	password: "hotel", // Change this to your PostgreSQL password
+	port: 5432, // Default PostgreSQL port
 });
 
 // login get request
@@ -43,25 +43,25 @@ const pool = new Pool({
 // }
 
 exports.getLogin = (req, res, next) => {
-  if (req.session.admin == undefined) {
-    res.render("admin/login", { msg: "", err: "" });
-  } else {
-    const data1 = "SELECT * FROM bookingstatus WHERE status = 0";
-    pool.query(data1, (err1, result1) => {
-      if (err1) throw err1;
-      else {
-        for (const row of result1.rows) {
-          const a = row.date;
-          row.date = a.toString().slice(0, 15);
-        }
-        return res.render("admin/index", {
-          msg: "",
-          err: "",
-          data: result1.rows,
-        });
-      }
-    });
-  }
+	if (req.session.admin == undefined) {
+		res.render("admin/login", { msg: "", err: "" });
+	} else {
+		const data1 = "SELECT * FROM bookingstatus WHERE status = 0";
+		pool.query(data1, (err1, result1) => {
+			if (err1) throw err1;
+			else {
+				for (const row of result1.rows) {
+					const a = row.date;
+					row.date = a.toString().slice(0, 15);
+				}
+				return res.render("admin/index", {
+					msg: "",
+					err: "",
+					data: result1.rows,
+				});
+			}
+		});
+	}
 };
 
 //login post request
@@ -113,39 +113,39 @@ exports.getLogin = (req, res, next) => {
 // };
 
 exports.postLogin = (req, res, next) => {
-  const data = "SELECT * FROM admin WHERE name = $1 AND pass = $2";
-  const values = [req.body.name, req.body.pass];
+	const data = "SELECT * FROM admin WHERE name = $1 AND pass = $2";
+	const values = [req.body.name, req.body.pass];
 
-  const data1 = "SELECT * FROM bookingstatus WHERE status = 0";
+	const data1 = "SELECT * FROM bookingstatus WHERE status = 0";
 
-  pool.query(data, values, (err, result) => {
-    if (err) throw err;
-    else {
-      if (result.rows.length) {
-        req.session.admin = result.rows[0].name;
-        pool.query(data1, (err1, result1) => {
-          if (err1) throw err1;
-          else {
-            console.log(result1.rows);
-            for (const row of result1.rows) {
-              const a = row.date;
-              row.date = a.toString().slice(0, 15);
-            }
-            return res.render("admin/index", {
-              msg: "",
-              err: "",
-              data: result1.rows,
-            });
-          }
-        });
-      } else {
-        return res.render("admin/login", {
-          msg: "",
-          err: "Please Check Your Information Again",
-        });
-      }
-    }
-  });
+	pool.query(data, values, (err, result) => {
+		if (err) throw err;
+		else {
+			if (result.rows.length) {
+				req.session.admin = result.rows[0].name;
+				pool.query(data1, (err1, result1) => {
+					if (err1) throw err1;
+					else {
+						console.log(result1.rows);
+						for (const row of result1.rows) {
+							const a = row.date;
+							row.date = a.toString().slice(0, 15);
+						}
+						return res.render("admin/index", {
+							msg: "",
+							err: "",
+							data: result1.rows,
+						});
+					}
+				});
+			} else {
+				return res.render("admin/login", {
+					msg: "",
+					err: "Please Check Your Information Again",
+				});
+			}
+		}
+	});
 };
 
 //change booking status
@@ -207,61 +207,59 @@ exports.postLogin = (req, res, next) => {
 // };
 
 exports.postChnageStatus = (req, res, next) => {
-  let value = 0;
-  let data;
-  let values = [
-    value,
-    req.body.mail,
-    req.body.type,
-    req.body.cat,
-    req.body.want,
-  ];
+	let value = 0;
+	let data;
+	let values = [
+		value,
+		req.body.mail,
+		req.body.type,
+		req.body.cat,
+		req.body.want,
+	];
+	if (req.body.click == "Approve") {
+		value = 1;
+		data =
+			"UPDATE bookingstatus " +
+			"SET status = $1 " +
+			"WHERE email = $2 AND type = $3 AND category = $4 AND roomWant = $5";
+	} else {
+		values = values.slice(1);
+		data =
+			"DELETE FROM bookingstatus " +
+			"WHERE email = $1 AND type = $2 AND category = $3 AND roomWant = $4";
+	}
 
-  if (req.body.click == "Approve") {
-    value = 1;
+	console.log("data");
+	console.log(data);
+	console.log("values");
+	console.log(values);
+	const data1 = "SELECT * FROM bookingstatus WHERE status = 0";
 
-    data =
-      "UPDATE bookingstatus " +
-      "SET status = $1 " +
-      "WHERE email = $2 AND type = $3 AND category = $4 AND roomWant = $5";
-  } else {
-    values = values.slice(1);
-    data =
-      "DELETE FROM bookingstatus " +
-      "WHERE email = $1 AND type = $2 AND category = $3 AND roomWant = $4";
-  }
-
-  console.log("data");
-  console.log(data);
-  console.log("values");
-  console.log(values);
-  const data1 = "SELECT * FROM bookingstatus WHERE status = 0";
-
-  pool.query(data, values, (err, result) => {
-    if (err) throw err;
-    else {
-      pool.query(data1, (err1, result1) => {
-        if (err1) throw err1;
-        else {
-          for (const row of result1.rows) {
-            var a = row.date;
-            row.date = a.toString().slice(0, 15);
-          }
-          return res.render("admin/index", {
-            msg: "",
-            err: "",
-            data: result1.rows,
-          });
-        }
-      });
-    }
-  });
+	pool.query(data, values, (err, result) => {
+		if (err) throw err;
+		else {
+			pool.query(data1, (err1, result1) => {
+				if (err1) throw err1;
+				else {
+					for (const row of result1.rows) {
+						var a = row.date;
+						row.date = a.toString().slice(0, 15);
+					}
+					return res.render("admin/index", {
+						msg: "",
+						err: "",
+						data: result1.rows,
+					});
+				}
+			});
+		}
+	});
 };
 
 //get add hotel page
 
 exports.getAddHotel = (req, res, next) => {
-  res.render("admin/addhotel", { msg: "", err: "" });
+	res.render("admin/addhotel", { msg: "", err: "" });
 };
 
 //add new hotel info
@@ -367,74 +365,74 @@ exports.getAddHotel = (req, res, next) => {
 // };
 
 exports.postAddHotel = (req, res, next) => {
-  let cat = "";
-  let type = "";
-  let cost = 0;
-  let avlvl = 0;
-  let des = "";
-  let imgPath = "";
-  let wrong = 0;
+	let cat = "";
+	let type = "";
+	let cost = 0;
+	let avlvl = 0;
+	let des = "";
+	let imgPath = "";
+	let wrong = 0;
 
-  new formidable.IncomingForm()
-    .parse(req)
-    .on("field", (name, field) => {
-      if (name === "cat") {
-        cat = field;
-      } else if (name === "type") {
-        type = field;
-      } else if (name === "cost") {
-        cost = parseInt(field);
-      } else if (name === "avlvl") {
-        avlvl = parseInt(field);
-      } else if (name === "des") {
-        des = field;
-      }
-    })
-    .on("fileBegin", function (name, file) {
-      const fileType = file.type.split("/").pop();
-      if (fileType === "jpg" || fileType === "png" || fileType === "jpeg") {
-        const uploadDir = path.join(__dirname, "../public/assets/img/rooms/");
-        imgPath = `/assets/img/rooms/${cat}${type}${cost}.${fileType}`;
-        file.path = path.join(uploadDir, `${cat}${type}${cost}.${fileType}`);
-      } else {
-        console.log("Wrong File type");
-        wrong = 1;
-        res.render("admin/addhotel", { msg: "", err: "Wrong File type" });
-      }
-    })
-    .on("aborted", () => {
-      console.error("Request aborted by the user");
-    })
-    .on("error", (err) => {
-      console.error("Error", err);
-      throw err;
-    })
-    .on("end", () => {
-      if (wrong === 1) {
-        console.log("Error");
-      } else {
-        const data =
-          "INSERT INTO category(name, type, cost, available, img, dec) " +
-          "VALUES($1, $2, $3, $4, $5, $6)";
-        const values = [cat, type, cost, avlvl, imgPath, des];
+	new formidable.IncomingForm()
+		.parse(req)
+		.on("field", (name, field) => {
+			if (name === "cat") {
+				cat = field;
+			} else if (name === "type") {
+				type = field;
+			} else if (name === "cost") {
+				cost = parseInt(field);
+			} else if (name === "avlvl") {
+				avlvl = parseInt(field);
+			} else if (name === "des") {
+				des = field;
+			}
+		})
+		.on("fileBegin", function (name, file) {
+			const fileType = file.type.split("/").pop();
+			if (fileType === "jpg" || fileType === "png" || fileType === "jpeg") {
+				const uploadDir = path.join(__dirname, "../public/assets/img/rooms/");
+				imgPath = `/assets/img/rooms/${cat}${type}${cost}.${fileType}`;
+				file.path = path.join(uploadDir, `${cat}${type}${cost}.${fileType}`);
+			} else {
+				console.log("Wrong File type");
+				wrong = 1;
+				res.render("admin/addhotel", { msg: "", err: "Wrong File type" });
+			}
+		})
+		.on("aborted", () => {
+			console.error("Request aborted by the user");
+		})
+		.on("error", (err) => {
+			console.error("Error", err);
+			throw err;
+		})
+		.on("end", () => {
+			if (wrong === 1) {
+				console.log("Error");
+			} else {
+				const data =
+					"INSERT INTO category(name, type, cost, available, img, dec) " +
+					"VALUES($1, $2, $3, $4, $5, $6)";
+				const values = [cat, type, cost, avlvl, imgPath, des];
 
-        pool.query(data, values, (err, result) => {
-          if (err) {
-            throw err;
-          } else {
-            res.render("admin/addhotel", {
-              msg: "Data Insert Successfuly",
-              err: "",
-            });
-          }
-        });
-      }
-    });
+				pool.query(data, values, (err, result) => {
+					if (err) {
+						throw err;
+					} else {
+						res.render("admin/addhotel", {
+							msg: "Data Insert Successfuly",
+							err: "",
+						});
+					}
+				});
+			}
+		});
 };
 
 //get update page
 exports.getSearch = (req, res, next) => {
-  res.render("admin/search", { msg: "", err: "" });
+	res.render("admin/search", { msg: "", err: "" });
 };
 
 //post request
@@ -462,19 +460,19 @@ exports.getSearch = (req, res, next) => {
 // };
 
 exports.postSearch = (req, res, next) => {
-  const data = "SELECT * FROM category WHERE name = $1";
-  const values = [req.body.cat];
+	const data = "SELECT * FROM category WHERE name = $1";
+	const values = [req.body.cat];
 
-  pool.query(data, values, (err, result) => {
-    if (err) throw err;
-    else {
-      return res.render("admin/update", {
-        msg: "",
-        err: "",
-        data: result.rows,
-      });
-    }
-  });
+	pool.query(data, values, (err, result) => {
+		if (err) throw err;
+		else {
+			return res.render("admin/update", {
+				msg: "",
+				err: "",
+				data: result.rows,
+			});
+		}
+	});
 };
 
 //get update page
@@ -508,17 +506,17 @@ exports.postSearch = (req, res, next) => {
 // };
 
 exports.getUpdate = (req, res, next) => {
-  const data =
-    "SELECT * FROM category " + "WHERE name = $1 AND type = $2 AND cost = $3";
-  const values = [req.body.cat, req.body.type, req.body.cost];
+	const data =
+		"SELECT * FROM category " + "WHERE name = $1 AND type = $2 AND cost = $3";
+	const values = [req.body.cat, req.body.type, req.body.cost];
 
-  pool.query(data, values, (err, result) => {
-    if (err) throw err;
-    else {
-      req.session.info = result.rows[0];
-      res.render("admin/updatePage", { data: result.rows[0] });
-    }
-  });
+	pool.query(data, values, (err, result) => {
+		if (err) throw err;
+		else {
+			req.session.info = result.rows[0];
+			res.render("admin/updatePage", { data: result.rows[0] });
+		}
+	});
 };
 
 //update previous data
@@ -557,31 +555,31 @@ exports.getUpdate = (req, res, next) => {
 // };
 
 exports.updatePrevData = (req, res, next) => {
-  const data =
-    "UPDATE category " +
-    "SET type = $1, cost = $2, available = $3, dec = $4 " +
-    "WHERE name = $5 AND type = $6 AND cost = $7";
+	const data =
+		"UPDATE category " +
+		"SET type = $1, cost = $2, available = $3, dec = $4 " +
+		"WHERE name = $5 AND type = $6 AND cost = $7";
 
-  const values = [
-    req.body.type,
-    parseInt(req.body.cost),
-    parseInt(req.body.avlvl),
-    req.body.des,
-    req.session.info.name,
-    req.session.info.type,
-    parseInt(req.session.info.cost),
-  ];
+	const values = [
+		req.body.type,
+		parseInt(req.body.cost),
+		parseInt(req.body.avlvl),
+		req.body.des,
+		req.session.info.name,
+		req.session.info.type,
+		parseInt(req.session.info.cost),
+	];
 
-  pool.query(data, values, (err, result) => {
-    if (err) throw err;
-    else {
-      res.render("admin/search", { msg: "Update Done Successfully", err: "" });
-    }
-  });
+	pool.query(data, values, (err, result) => {
+		if (err) throw err;
+		else {
+			res.render("admin/search", { msg: "Update Done Successfully", err: "" });
+		}
+	});
 };
 
 //logout
 exports.logout = (req, res, next) => {
-  req.session.destroy();
-  res.render("admin/login", { msg: "", err: "" });
+	req.session.destroy();
+	res.render("admin/login", { msg: "", err: "" });
 };
